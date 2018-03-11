2dbcbf12762fee96f0718614f103416a0270be39
wujiajia <979226469@qq.com>
2018-03-11 22:54:48 +0800

    呵呵

--- js/index.js
+++ js/index.js
@@ -33,9 +33,37 @@ function errorModal(tip){
 
     });
 };
-$.cookie('searchLawyer', '');//找律师
-$.cookie('searchCase', '');//查案件
 
+function caseFoud(caseDes, callback) {
+	var Data = JSON.stringify({
+		"text": caseDes
+	});
+	$.ajax({
+		dataType: 'json',
+		url: 'http://47.92.38.167:8889/feature_query/case_type', // http://47.92.38.167:8888/  http://47.92.38.167:8889
+		type: 'post',
+		data: Data,
+		success: function(res) {
+			if(res.code == 0 && res.data) { //表示请求成功
+				var reason = res.data[0].reason,
+					num = "reason_" + res.data[0].sub_reason_class;
+				var obj = {
+					"reason2": res.data[0].second_reason
+				}
+				obj[num] = reason;
+
+				if(callback) {
+					callback(JSON.stringify(obj));
+				}
+			} else {
+				alert("查询失败，错误代码：code=" + res.code + res.msg);
+			}
+		},
+		error: function() {
+			console.error('/static_query/lawyer_list', arguments);
+		}
+	});
+}
 
 
 
