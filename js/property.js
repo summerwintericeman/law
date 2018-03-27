b1dced5ca5837aba39be51ef8b9b3b9c11a15bde
sphwjj <1144621000@qq.com>
2018-03-27 21:51:02 +0800

    还原

--- js/property.js
+++ js/property.js
@@ -116,6 +116,15 @@ $(document).ready(function() {
 
         //cpquery/doc_url
         if(patentNum){
+            //验证专利号是否正确
+            var reg1 = /^(CN)*[1-9]\d{3}(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])\d{4}\.[0-9]$/,
+                reg2 = /^[1-9]\d{3}(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])\d{4}[0-9]$/;
+            if(!(reg1.test(patentNum) || reg2.test(patentNum))){
+                //验证错误
+                errorModal('专利号输入有误，请重新填写！');
+                return;
+            }
+
             var obj = JSON.stringify({
                 patent_no:patentNum
             });
