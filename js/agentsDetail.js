d4286fe652d148f6aba6f24c0cb1b095cf10e57f
sphwjj <1144621000@qq.com>
2018-03-29 23:22:11 +0800

    代理人详情

--- js/agentsDetail.js
+++ js/agentsDetail.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
 	var getPer = getUrlParam('per', true) || '';
 	//var getCom = getUrlParam('com',true) || '';
 	var parNode = $('.content');
-	console.log(getPer);
+	var agentMsg = $.cookie('agentBaseMsg');
+    agentMsg = agentMsg?JSON.parse(agentMsg):'';
 	var _href = window.location.href;
 	if(_href.indexOf('&com') > -1) {
 		$('.path').find('a').eq(1).hide().next('i').hide();
@@ -24,7 +25,7 @@ $(document).ready(function() {
 			data: JSON.stringify(param),
 			success: function(res) {
 				console.log(res);
-				//showlist(res);
+				showlist(res);
 			},
 			error: function() {
 				errorModal('查询代理人详情失败!');
@@ -33,37 +34,73 @@ $(document).ready(function() {
 		});
 	};
 
-	agentsDetail(function(res) {
-		console.log(res);
-		if(res.code == 0 && res.data.data.length > 0) {
-			parNode.empty();
-			//遍历数组进行添加
-			var nameNode = `<div class='eachContent'><h3><span>${getPer}</span>`;
-			parNode.append(nameNode);
-			for(var i = 0; i < 3; i++) {
-				var obj = res.data.data[i];
-				console.log(obj)
-				var addNode = `<div class='eachContent'><h3><span></span>
-					<span><i class="LowTitle">${obj.dev_name}</i> </span></h3>
+
+
+	//添加基本信息节点
+	(function(){
+        var node1 = `<div class="clearfix msgWrap">
+						<div class="pull-left userMsg">
+							<img src="../img/default-big.jpg" onerror="this.src='../img/default-big.jpg'">
+							</div>
+							<div class="pull-left">
+						<h3><span>${getPer}　　</span><span>事务所：${agentMsg.cp_name || '--'}</span></h3>
+						<div class="lawyerMsg clearfix">
+						<p><span>执业证号</span><i>${agentMsg.certNo || '--'}</i></p>
+						<p><span>性别</span><i>${agentMsg.gender || '--'}</i></p>
+						<p class="zhuanliNum"><span>专利条数</span><i></i></p>
+						</div>
+						</div>
+						</div>
+						 <div class="caseType">
+						<p>
+						<i></i>
+						<span class="text-strong font-16 " id = 'dowell'>擅长领域:</span>
+						</p>
+						 <p>
+						<i></i>
+						<span class="text-strong font-16">专利展示:</span>
+						<ul class="node2List"></ul>
+						</p>
+						</div>`;
+        parNode.append(node1);
+
+        agentsDetail(function(res) {
+            if(res.code == 0 && res.data.data.length > 0) {
+            	//专利条数
+               $('.msgWrap p.zhuanliNum i').html(res.data.count);
+               //擅长领域
+				var dowellHtml = $('#dowell').html();
+                $('#dowell').html(dowellHtml + '此处暂无数据');
+
+                //遍历数组进行添加
+                for(var i = 0; i < 3; i++) {
+                    var obj = res.data.data[i];
+                    var addNode = `<li class='eachContent'><h3><span></span>
+					<span><i class="LowTitle text-strong">${obj.dev_name}</i> </span></h3>
                     <p class="com">
-                        <span><i class="LowTitle">专利类型:</i> ${obj.patent_type}</span>
+                        <span><i class="LowTitle text-strong">专利类型:</i> ${obj.patent_type}</span>
                         <br/>
-                        <span><i class="LowTitle">专利描述:</i> ${obj.abstract}</span>
+                        <span><i class="LowTitle text-strong">专利描述:</i> ${obj.abstract}</span>
                         <br/>
-                    <span><i class="LowTitle">专利号/日期:</i> ${obj.patent_no} / ${obj.public_date}</span>
-                    </p>
-                    <div class="caseType">
-                       
-                    </div> </div>`;
-				parNode.append(addNode);
-			}
+                    <span><i class="LowTitle text-strong">专利号/日期:</i> ${obj.patent_no} / ${obj.public_date}</span>
+                    </p> </li>`;
+                    $('ul.node2List').append(addNode);
+                }
 
-		} else {
-			errorModal(res.msg);
-			console.error('查询代理人详情失败:', res);
-		}
 
-	});
+
+            } else {
+                errorModal(res.msg);
+                console.error('查询代理人详情失败:', res);
+            }
+
+        });
+
+
+
+
+	})();
+
 	//添加案件列表子节点
 	//	function createPage(data) {
 	//		debugger;
